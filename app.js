233c09a8b0f6ec05f5b1f4dd838e158d624412f7
Your Name <you@example.com>
2019-12-17 10:56:59 -0600

    playstore server

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const morgan = require('morgan')
+const playstore = require('./playstore')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(morgan('dev'))
+
+app.get('/apps', (req, res) => {
+    const {search = "", sort, genres} = req.query
+
+    let results = playstore
+        .filter(app => 
+            app
+                .App 
+                .toLowerCase()
+                .includes(search.toLowerCase()))
+
+    let genresType = playstore
+            .filter(app =>
+                app
+                    .Genres
+                    .includes(genres))
+
+    if(genres) {
+        return genresType.sort
+    }
+
+    if(genres) {
+        if(!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(genres)) {
+            return res
+                .status(400)
+                .send('Genres must be one of Action, Puzzle, Strategy, Casual, Arcade, Card')
+        }
+    }
+
+    if(sort) {
+        results
+            .sort((a, b) => {
+                return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0
+            })
+    }
+
+    if(sort) {
+        if(!['Rating', 'App'].includes(sort)) {
+            return res
+                .status(400)
+                .send('Sort must be one of rating or app')
+        }
+    }
+    res.json(results)
+})
+
+app.listen(8000, () => {
+    console.log('Server started on PORT 8000')
+})
